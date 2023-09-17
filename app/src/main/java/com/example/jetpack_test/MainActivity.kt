69e2d1eda84767fbd27f0a4d72db45dea89f76c2
EtherialSpirit <etherialspirit@gmail.com>
2023-09-17 20:33:07 +0200

    Card&Box

--- app/src/main/java/com/example/jetpack_test/MainActivity.kt
+++ app/src/main/java/com/example/jetpack_test/MainActivity.kt
@@ -3,56 +3,123 @@ package com.example.jetpack_test
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.modifier.modifierLocalMapOf
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.jetpack_test.ui.theme.JetpacktestTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-                Row(modifier = Modifier
-                    .background(Color.Cyan)
-                    .fillMaxSize()
-                    .fillMaxHeight(),
-                    horizontalArrangement = Arrangement.SpaceEvenly) {
-                    Column(modifier = Modifier
-                        .background(Color.Red)
-                        .fillMaxSize(0.3f)
-                        .fillMaxHeight(),
-                        verticalArrangement = Arrangement.SpaceEvenly,
-                        horizontalAlignment = Alignment.CenterHorizontally) {
-                        Text(text = "Hello")
-                        Text(text = "Privet")
-                        Text(text = "Alloha")
-                    }
-                    Column(modifier = Modifier
-                        .background(Color.Green)
-                        .fillMaxSize(0.5f)
-                        .fillMaxHeight(),
-                        verticalArrangement = Arrangement.SpaceEvenly,
-                        horizontalAlignment = Alignment.CenterHorizontally) {
-                        Text(text = "Hello")
-                        Text(text = "Privet")
-                        Text(text = "Alloha")
-                    }
+            //RowColumn()
+            Column {
+                ListItem(name = "Artem Bolotov", prof ="1C" )
+                ListItem(name = "Artem Bolotov", prof ="1C" )
+                ListItem(name = "Artem Bolotov", prof ="1C" )
+                ListItem(name = "Artem Bolotov", prof ="1C" )
+                ListItem(name = "Artem Bolotov", prof ="1C" )
 
-                }
             }
+
         }
     }
+}
+
+@Composable
+private fun ListItem(name: String, prof: String) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp),
+        shape = RoundedCornerShape(15.dp),
+        elevation = CardDefaults.cardElevation(5.dp)
+    ) {
+        Box() {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.image),
+                    contentDescription = "image",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .padding(5.dp)
+                        .size(64.dp)
+                        .clip(CircleShape)
+                )
+                Column(modifier = Modifier.padding(16.dp)
+                ) {
+                    Text(text = name)
+                    Text(text = prof)
+                }
+
+            }
+
+        }
+    }
+}
+
+@Composable
+private fun RowColumn() {
+    Row(
+        modifier = Modifier
+            .background(Color.Cyan)
+            .fillMaxSize()
+            .fillMaxHeight(),
+        horizontalArrangement = Arrangement.SpaceEvenly
+    ) {
+        Column(
+            modifier = Modifier
+                .background(Color.Red)
+                .fillMaxSize(0.3f)
+                .fillMaxHeight(),
+            verticalArrangement = Arrangement.SpaceEvenly,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "Hello")
+            Text(text = "Privet")
+            Text(text = "Alloha")
+        }
+        Column(
+            modifier = Modifier
+                .background(Color.Green)
+                .fillMaxSize(0.5f)
+                .fillMaxHeight(),
+            verticalArrangement = Arrangement.SpaceEvenly,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "Hello")
+            Text(text = "Privet")
+            Text(text = "Alloha")
+        }
+
+    }
+}
 
